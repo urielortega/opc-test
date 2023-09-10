7550eb2918b944aeb124596f19da6b7d4ad3febb
Uriel Ortega <urielortega2509@gmail.com>
2023-09-10 11:08:32 -0600

    Initial Commit

---
+++ OpenClosedPrinciple/OpenClosedPrincipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClosedPrincipleApp.swift
+//  OpenClosedPrinciple
+//
+//  Created by Uriel Ortega on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenClosedPrincipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
