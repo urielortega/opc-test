2ec789cc3e91d8d00e022d532f15689e5b678dc1
Uriel Ortega <urielortega2509@gmail.com>
2023-09-10 13:56:02 -0600

    Implemented basic layout.

--- OpenClosedPrinciple/ContentView.swift
+++ OpenClosedPrinciple/ContentView.swift
@@ -7,17 +7,33 @@
 
 import SwiftUI
 
+struct ClockView: View {
+    let hours = "00"
+    let minutes = "00"
+    let seconds = "00"
+
+    var body: some View {
+        HStack {
+            Text(hours)
+            Text(":")
+            Text(minutes)
+            Text(":")
+            Text(seconds)
+        }
+        .font(.largeTitle)
+        .monospacedDigit()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ClockView()
         }
         .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
